4a18a6ddd8b467d510aa0a01c6fb1f6c30431caf
Zac Banas <36123643+ZacThePaul@users.noreply.github.com>
2021-09-02 20:09:33 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import praw
+import time
+
+reddit = praw.Reddit(client_id='dO4fTH1iyzrhAHvrk1AHyQ',
+                     client_secret='qbemHJv537faCzxF38d4fEsAxqDcQw',
+                     user_agent='redditdev scraper by u/T_CLAVDIVS_CAESAR',
+                     username='T-CLAVDIVS-CAESAR',
+                     password='Banaszac17!')
+
+ask_historians = reddit.subreddit('AskHistorians')
+
+mod_list = []
+
+for moderator in ask_historians.moderator():
+    mod_list.append(moderator)
+
+for submission in ask_historians.comments(limit=25):
+    for mod in mod_list:
+        if submission.author == mod:
+            print(submission.author)
